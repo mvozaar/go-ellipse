f383a3b759f2776500c909f0f25a31ff08ea5e76
mvozaar <mvozaar@hotmail.com>
2019-06-20 23:26:36 +0200

    typos repaired

--- Ellipse.go
+++ Ellipse.go
@@ -9,6 +9,7 @@ type Init struct {
 	A, B float64
 }
 
-func (e* Init) GetEccecntricity() float64 {
+func (e* Init) GetEccentricity() float64 {
+	fmt.Println("")
 	return (math.Sqrt(math.Pow(e.A, 2) - math.Pow(e.B, 2))) / e.A
 }
